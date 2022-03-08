38d425865d47ef2c6ec0a55b6809555cb81cfdd5
huang <mains2114@gmail.com>
2022-03-09 04:23:35 +0800

    新增二叉树打印方法

--- src/main/java/play/util/BinTree.java
+++ src/main/java/play/util/BinTree.java
@@ -1,12 +1,10 @@
 package play.util;
 
+import com.google.common.collect.Lists;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.TreeMap;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
@@ -39,15 +37,14 @@ public class BinTree {
             this.parent = parent;
         }
 
-    @Override
-    public String toString() {
-        return "Node{" +
-            "val=" + val +
-            ", left=" + left +
-            ", right=" + right +
-            '}';
+        @Override
+        public String toString() {
+            return "{" + val +
+                ", L=" + left +
+                ", R=" + right +
+                '}';
+        }
     }
-}
 
     public static void preOrder(BinTree.Node root, List<Integer> result) {
         if (root == null) {
@@ -148,6 +145,68 @@ public class BinTree {
         }
     }
 
+    public static void levelPrint(Node root) {
+        List<List<Integer>> lines = new ArrayList<>();
+
+        Node empty = new Node(-1);
+
+        int height = 0;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        lines.add(Lists.newArrayList(root.val));
+        boolean hasChild = root.left != null || root.right != null;
+        while (!queue.isEmpty() && hasChild) {
+            height++;
+            hasChild = false;
+            ArrayList<Integer> line = new ArrayList<>();
+            int queueSize = queue.size();
+            for (int i = 0; i < queueSize; i++) {
+                Node tmp = queue.poll();
+                if (tmp == null) {
+                    break;
+                }
+                // result.add(tmp.getVal());
+                if (tmp.getLeft() != null) {
+                    queue.offer(tmp.getLeft());
+                    line.add(tmp.left.val);
+                    hasChild = true;
+                } else {
+                    queue.offer(empty);
+                    line.add(-1);
+                }
+
+                if (tmp.getRight() != null) {
+                    queue.offer(tmp.getRight());
+                    line.add(tmp.right.val);
+                    hasChild = true;
+                } else {
+                    queue.offer(empty);
+                    line.add(-1);
+                }
+            }
+            lines.add(line);
+        }
+
+        ArrayList<String> lines2 = new ArrayList<>();
+        for (int i = 0; i < lines.size(); i++) {
+            StringBuilder sb = new StringBuilder();
+            if (i > 0) {
+                sb.append(String.format("%"+((2<<i-1)-1)+"s", ""));
+            }
+            List<Integer> tmpLine = lines.get(lines.size() - i - 1);
+            for (int j = 0; j < tmpLine.size(); j++) {
+                sb.append(formatInt(tmpLine.get(j))).append(String.format("%"+((2<<i)-1)+"s", ""));
+            }
+            lines2.add(0, sb.toString());
+        }
+
+        System.out.println(String.join("\n", lines2));
+    }
+
+    private static String formatInt(int i) {
+        return i == -1 ? "#" : i + "";
+    }
+
     public static void insert(Node root, int val) {
         if (root == null) {
             return;
@@ -172,6 +231,87 @@ public class BinTree {
         }
     }
 
+    public static Node search(Node root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        Node t = root;
+        do {
+            if (val < t.getVal()) {
+                t = t.getLeft();
+            } else if (val > t.getVal()) {
+                t = t.getRight();
+            } else {
+                return t;
+            }
+        } while (t != null);
+
+        return null;
+    }
+
+    public static void remove(Node root, int val) {
+        Node target = search(root, val);
+        if (target == null) {
+            return;
+        }
+
+        if (target.right == null) {
+            if (target.parent.left == target) {
+                target.parent.left = target.left;
+            } else {
+                target.parent.right = target.left;
+            }
+            if (target.left != null) {
+                target.left.parent = target.parent;
+            }
+        } else if (target.left == null) {
+            if (target.parent.left == target) {
+                target.parent.left = target.right;
+            } else {
+                target.parent.right = target.right;
+            }
+            target.right.parent = target.parent;
+        } else {
+            // 找到前置节点
+            Node t = target.left;
+            while (t.right != null) {
+                t = t.right;
+            }
+
+            // 将前置节点的值复制到当前节点
+            target.val = t.val;
+            // 将前置节点的左子树添加到父节点，替换前置节点的位置
+            if (t == t.parent.left) {
+                t.parent.left = t.left;
+            } else {
+                t.parent.right = t.left;
+            }
+            if (t.left != null) {
+                t.left.parent = t.parent;
+            }
+
+            // 释放t
+            t.left = t.parent = null;
+        }
+    }
+
+    public static void rotateRight(Node t) {
+        Node top = t.left;
+
+        t.left = top.right;
+        top.right = t;
+        top.parent = t.parent;
+        if (t.parent != null) {
+            if (t.parent.left.equals(t)) {
+                t.parent.left = top;
+            } else {
+                t.parent.right = top;
+            }
+        }
+        t.parent = top;
+    }
+
     public static void main(String[] args) {
 //        testOrder();
         testInsert();
@@ -187,6 +327,14 @@ public class BinTree {
         List<Integer> result = new ArrayList<>();
         inOrderStack(root, result);
         log.info("inOrder = {}", result);
+
+        remove(root, 3);
+        log.info("tree = {}", root);
+        result.clear();
+        levelOrder(root, result);
+        log.info("levelOrder = {}", result);
+
+        levelPrint(root);
     }
 
     private static void testOrder() {
